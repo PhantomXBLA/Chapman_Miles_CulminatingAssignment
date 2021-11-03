4c6afc01455ee7782d7623ff29ce0614ced1cd20
Tyler <phantomxbla@gmail.com>
2021-11-03 14:24:57 -0400

    Started work on the Enum database for the monsters

---
+++ Assets/Scripts/ScendoEnums.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+
+public enum Scendo
+{
+    SpeedStat = 0,
+    HealthStat = 0,
+    AttackStat = 0,
+    DefenseStat = 0,
+    EXP = 0,
+    Level = 1
+}
