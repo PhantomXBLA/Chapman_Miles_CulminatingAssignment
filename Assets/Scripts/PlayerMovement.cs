5a41de6a2f385f7b29aa6b3f2e660d1246213ef8
Ian Chapman <e-n_chapman123@hotmail.com>
2021-11-16 15:05:12 -0500

    Needing a isMoving Bool for random encounters

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rigidbody = null;
 
-    public bool isMoving = false;
+    private bool isMoving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +22,36 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerInput();
+    }
 
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
+    void PlayerInput()
+    {
+            float inputX = Input.GetAxisRaw("Horizontal");
+            float inputY = Input.GetAxisRaw("Vertical");
 
-        rigidbody.velocity = new Vector2(inputX * moveSpeed, inputY * moveSpeed);
+            rigidbody.velocity = new Vector2(inputX * moveSpeed, inputY * moveSpeed);
+
+
+    }
+
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if ((other.tag == "Tall Grass") && (moveSpeed < 1.5f)) //try new standingStill bool
+        {
+            //A 1/1000 chance to encounter something if the player stays in the Tall Grass
+            //for any amount of time.
+            if (Random.Range(1, 101) <= 10)
+            {
+                Debug.Log("A wild something something appeared");
+                //Freeze the players movement until battle state is false.
+                //Play encounter animation and music
+                //Need to trigger battle state
+                //SceneManager.LoadScene("Battle Scene");
+            }
+            //if battle/random encounter scene is active, cannot encounter anything
+        }
     }
 }
--- Assets/Scripts/RandomEncounterSystem.cs
+++ Assets/Scripts/RandomEncounterSystem.cs
@@ -1,40 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-//https://www.youtube.com/watch?v=fePYYZaesSM&t=25s - Inspiration for Random Encounter System
+////https://www.youtube.com/watch?v=fePYYZaesSM&t=25s - Inspiration for Random Encounter System
 
-public class RandomEncounterSystem : MonoBehaviour
-{
+//public class RandomEncounterSystem : MonoBehaviour
+//{
 
-    // Start is called before the first frame update
-    void Start()
-    {
+//    // Start is called before the first frame update
+//    void Start()
+//    {
 
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
+//    }
 
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.tag == "Tall Grass") //and is moving
-        {
-            //A 1/1000 chance to encounter something if the player stays in the Tall Grass
-            //for any amount of time.
-            if (Random.Range(1, 1001) <= 1)
-            {
-                Debug.Log("A wild something something appeared");
-                //Freeze the players movement until battle state is false.
-                //Play encounter animation and music
-                //Need to trigger battle state
-                //SceneManager.LoadScene("Battle Scene");
-            }
-            //if battle/random encounter scene is active, cannot encounter anything
-        }
-    }
-}
+//    private void OnTriggerStay2D(Collider2D other)
+//    {
+//        if (other.tag == "Tall Grass") //and is moving
+//        {
+//            //A 1/1000 chance to encounter something if the player stays in the Tall Grass
+//            //for any amount of time.
+//            if (Random.Range(1, 1001) <= 1)
+//            {
+//                Debug.Log("A wild something something appeared");
+//                //Freeze the players movement until battle state is false.
+//                //Play encounter animation and music
+//                //Need to trigger battle state
+//                //SceneManager.LoadScene("Battle Scene");
+//            }
+//            //if battle/random encounter scene is active, cannot encounter anything
+//        }
+//    }
+//}
