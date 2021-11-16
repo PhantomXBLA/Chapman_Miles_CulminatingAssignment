0468a4329f6cddfc661694abe5afb05dec7fbe39
Tyler <phantomxbla@gmail.com>
2021-11-16 14:01:35 -0500

    Changed ScendoEnum to a class as it would be more flexible

---
+++ Assets/Scripts/ScendoClass.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+
+public class ScendoClass
+{
+    int SpeedStat = 0;
+    int HealthStat = 0;
+    int AttackStat = 0;
+    int DefenseStat = 0;
+    int EXP = 0;
+    int Level = 1;
+
+    string name;
+
+    Sprite frontSprite;
+    Sprite backSprite;
+
+
+
+}
