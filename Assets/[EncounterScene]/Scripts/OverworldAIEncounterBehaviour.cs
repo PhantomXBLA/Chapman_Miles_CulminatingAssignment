c79e735be962a1bf33617eb82b924d9fce246a1a
Ian Chapman <e-n_chapman123@hotmail.com>
2021-12-02 05:30:28 -0500

    Fade to black functioning with sound effects on AI encounter

---
+++ Assets/[EncounterScene]/Scripts/OverworldAIEncounterBehaviour.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OverworldAIEncounterBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> tapeSoundClips;
+
+    public GameObject player;
+
+    public Image blackFade;
+
+    public AudioSource tapeSounds;
+
+    public GameObject spacebarIcon;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //For fading from black on stage opening
+        blackFade.canvasRenderer.SetAlpha(1.0f);
+
+        // For Fading to black
+        blackFade.canvasRenderer.SetAlpha(0.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if ((other.tag == "Noah") && (Input.GetKeyDown("space")))
+        {
+            //disable player movement and animations
+            player.GetComponent<PlayerMovement>().enabled = !player.GetComponent<PlayerMovement>().enabled;
+            player.GetComponent<CharacterWalkAnimController>().enabled = !player.GetComponent<CharacterWalkAnimController>().enabled;
+
+            //turn off the space bar icon
+            spacebarIcon.SetActive(false);
+            
+
+            StartCoroutine(DelayBeforeEncounter());
+        }
+
+    }
+
+    public void fadeToBlack()
+    {
+        //This is changing the FadeIn/Out image to 1 (0 = invisible / 1 = visible)
+        // 2nd argument is the amount of time the fade takes to complete
+        blackFade.CrossFadeAlpha(1, 3.0f, false);
+
+    }
+
+    IEnumerator DelayAndFadeToBlack()
+    {
+        //wait 2 seconds for tape sound
+        yield return new WaitForSeconds(2);
+        fadeToBlack();
+
+        
+    }
+
+    IEnumerator ReActivateMovementAndAnimations()
+    {
+        yield return new WaitForSeconds(5);
+        //Re-enable player movement
+        player.GetComponent<PlayerMovement>().enabled = !player.GetComponent<PlayerMovement>().enabled;
+        player.GetComponent<CharacterWalkAnimController>().enabled = !player.GetComponent<CharacterWalkAnimController>().enabled;
+    }
+
+    IEnumerator WaitToPlayOtherSFX()
+    {
+        yield return new WaitForSeconds(.4f);
+        tapeSounds.clip = tapeSoundClips[1];
+        tapeSounds.Play();
+    }
+
+    IEnumerator WaitToStartEncounter()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("EncounterScene");
+        
+        
+    }
+
+    IEnumerator DelayBeforeEncounter()
+    {
+        //First the text prints
+        //And we give the player 5 seconds to read it
+        Debug.Log("DelayBeforeEncounter has started");
+        yield return new WaitForSeconds(5.0f);
+        
+        
+        //play the button press
+        tapeSounds.clip = tapeSoundClips[0];
+        tapeSounds.Play();
+        StartCoroutine(WaitToPlayOtherSFX());
+        StartCoroutine(WaitToStartEncounter());
+        StartCoroutine(DelayAndFadeToBlack());
+        StartCoroutine(ReActivateMovementAndAnimations());
+ 
+
+        //load scene
+
+    }
+
+}
